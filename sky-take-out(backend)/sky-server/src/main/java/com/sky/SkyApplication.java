203286ce40bb943e77bb5d8077e13fbdeeeae8a5
nuozhen <1543639703@qq.com>
2024-06-14 01:06:00 +0800

    project initialization code

--- sky-take-out(backend)/sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-take-out(backend)/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -11,6 +11,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
-        log.info("server started");
+        log.info("\n" +
+                "      _            _        _                      _   \n" +
+                "     | |          | |      | |                    | |  \n" +
+                "  ___| | ___   _  | |_ __ _| | _____    ___  _   _| |_ \n" +
+                " / __| |/ / | | | | __/ _` | |/ / _ \\  / _ \\| | | | __|\n" +
+                " \\__ \\   <| |_| | | || (_| |   <  __/ | (_) | |_| | |_ \n" +
+                " |___/_|\\_\\\\__, |  \\__\\__,_|_|\\_\\___|  \\___/ \\__,_|\\__|\n" +
+                "            __/ |                                      \n" +
+                "           |___/                                       " +
+                "\n" +
+                "server started");
     }
 }
